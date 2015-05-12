00e869c70850a9bf45c94719c78ad5f692394593
Jeff Saracco <jeffsaracco@gmail.com>
2015-05-12 09:17:48 -0400

    Update syntax for module instance methods

--- lib/hunt.rb
+++ lib/hunt.rb
@@ -25,13 +25,11 @@ module Hunt
     end
   end
 
-  module InstanceMethods
-    def concatted_search_values
-      self.class.search_keys.map { |key| send(key) }.flatten.join(' ')
-    end
+  def concatted_search_values
+    self.class.search_keys.map { |key| send(key) }.flatten.join(' ')
+  end
 
-    def index_search_terms
-      self.searches['default'] = Util.to_stemmed_words(concatted_search_values)
-    end
+  def index_search_terms
+    self.searches['default'] = Util.to_stemmed_words(concatted_search_values)
   end
 end
